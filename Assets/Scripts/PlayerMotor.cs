2f4e152038e6870903e784fda85e799c7ff2b7e0
Josue <sjosue5082002@gmail.com>
2023-09-23 13:18:16 -0500

    Movimiento relativoa camara

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    public PlayerInput input;
     public float horizontalMove;
     public float verticalMove;
     public float speed = 5f;
     public CharacterController player;
+    public Camera mainCamera;
+    private Vector3 playerInput;
+    private Vector3 camForward;
+    private Vector3 camRight;
+    private Vector3 movePlayer;
     void Start()
     {
         player = GetComponent<CharacterController>();
-    }
+    } 
 
-    // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
-        
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        player.Move(movePlayer * speed * Time.deltaTime);   
     }
 
-    public void FixedUpdate(){
-        player.Move(new Vector3(horizontalMove, 0, verticalMove) * speed * Time.deltaTime);
+    void camDirection(){
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 }
